78ee10483eb4f0aded500012aa4a172d3ced6b10
Thiago Gomes de Araújo <Thiagi-araujo@hotmail.com.br>
2021-10-30 22:00:49 -0300

    Resolvi o Erro do Knex

---
+++ src/migrations/20211031004855_create_users.js
@@ -0,0 +1,14 @@
+// const { default: knex } = require('knex');
+
+exports.up = (knex) => {
+  return knex.schema.createTable('users', (t) => {
+    t.increments('id').primary();
+    t.string('name').notNull();
+    t.string('mail').notNull().unique();
+    t.string('passwd').notNull();
+  });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('users');
+};
