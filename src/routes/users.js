6416144d9fb9f53fe3ef64de03e140a006a593d5
Thiago Gomes de Araujo <thiagi-araujo@hotmail.com.br>
2022-07-10 18:39:03 -0300

    Finalizacao Modulo 4

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,13 @@
 module.exports = (app) => {
   const findAll = (req, res) => {
-    app.db('users').select()
+    app.services.user.findAll()
       .then((result) => res.status(200).json(result));
   };
 
   const create = async (req, res) => {
-    const result = await app.db('users').insert(req.body, '*'); // Quando Passado um segundo parâmetro no Knex é para informar qual é o retorno daquela Execução (não funciona no MySQL)
+    const result = await app.services.user.save(req.body); // Quando Passado um segundo parâmetro no Knex é para informar qual é o retorno daquela Execução (não funciona no MySQL)
+    if (result.error) return res.status(400).json(result);
+
     res.status(201).json(result[0]);
   };
 
